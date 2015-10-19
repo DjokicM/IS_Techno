29ee7400bf328304d04a83d8cbc2d3fc4b2ea349
DjokicM <djokic.m@gmx.at>
2015-10-19 10:07:10 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main
+{
+    public static void main( String[] args )
+    {        
+      
+      System.out.println( "hello world from example resource" );
+    }
+}
